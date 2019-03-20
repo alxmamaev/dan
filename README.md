a5ec7d54d00aab7cae2c708939f7c0d4e2833778
Alexander Mamaev <alxmamaev@ya.ru>
2019-03-20 19:09:21 +0300

    Update

--- README.md
+++ README.md
@@ -9,12 +9,13 @@ from dan import DAN
 
 vocab_size = 100000
 vector_size = 300
+hidden_size = 300
 num_classes = 5
 
 batch_size = 32
 sequence_len = 100
 
-dan = DAN(vocab_size, vector_size, num_classes)
+dan = DAN(vocab_size, vector_size, hidden_size, num_classes)
 input_data = torch.randint(0, vocab_size, (batch_size, sequence_len))
 output = dan(input_data)
 
--- model.py
+++ model.py
@@ -4,11 +4,13 @@ from torch import nn
 from torch.distributions.bernoulli import Bernoulli
 
 class DAN(nn.Module):
-    def __init__(self, vocab_size, vector_size, output_size, dropout=0.3):
+    def __init__(self, vocab_size, vector_size,
+                hidden_size, output_size, dropout=0.3):
         super().__init__()
         self.bernoulli = Bernoulli(dropout)
         self.emb = nn.Embedding(vocab_size, vector_size)
-        self.fc = nn.Linear(vector_size, output_size)
+        self.fc1 = nn.Linear(vector_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
 
     def forward(self, X):
         mask = self.bernoulli.sample(X.shape).float().unsqueeze(2)
@@ -18,5 +20,7 @@ class DAN(nn.Module):
         out = out.sum(dim=1)
         out = out/(mask.sum(dim=1))
 
-        out = self.fc(out)
+        out = self.fc1(out)
+        out = self.fc2(out)
+
         return out
