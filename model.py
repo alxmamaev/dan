6119bffd14c7c08e1276d00c5d961bd52f8d515b
Alexander Mamaev <alxmamaev@ya.ru>
2019-04-04 16:28:17 +0300

    BatchNorm -> LayerNorm

--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 import torch
 from torch import nn
-from torch.distributions.bernoulli import Bernoulli
 
 class DAN(nn.Module):
     def __init__(self, vocab_size, vector_size,
@@ -8,13 +7,12 @@ class DAN(nn.Module):
         super().__init__()
         self.emb = nn.Embedding(vocab_size, vector_size)
 
-        self.enc = nn.Sequential(nn.BatchNorm1d(vector_size),
+        self.enc = nn.Sequential(nn.LayerNorm(vector_size),
                                  nn.Linear(vector_size, hidden_size),
-                                 nn.BatchNorm1d(hidden_size),
+                                 nn.LayerNorm(hidden_size),
                                  nn.Dropout(p=dropout),
                                  nn.ReLU(),
-                                 nn.Linear(hidden_size, output_size),
-                                 nn.BatchNorm1d(output_size))
+                                 nn.Linear(hidden_size, output_size))
 
     def forward(self, X):
         out = self.emb(X)
